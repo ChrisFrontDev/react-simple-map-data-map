ca0baa10e9ed5b49e3261838171dd41f3ca14520
Christian Santos <christiangraca@hotmail.com>
2021-02-03 14:13:10 -0300

    Dinamic Regions(wip)

--- src/MapChart.js
+++ src/MapChart.js
@@ -1,53 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import { scaleQuantize } from "d3-scale";
-import { csv } from "d3-fetch";
+import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps";
+import brasil from "./brasilTopoJson.json";
 
-const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
+const geoUrl = brasil
+const markers = [
 
-const colorScale = scaleQuantize()
-  .domain([1, 10])
-  .range([
-    "#ffedea",
-    "#ffcec5",
-    "#ffad9f",
-    "#ff8a75",
-    "#ff5533",
-    "#e2492d",
-    "#be3d26",
-    "#9a311f",
-    "#782618"
-  ]);
+  { markerOffset: 15, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
+
+];
 
 const MapChart = () => {
-  const [data, setData] = useState([]);
+  const [region, setRegion] = useState("nordeste");
 
   useEffect(() => {
-    // https://www.bls.gov/lau/
-    csv("/unemployment-by-county-2017.csv").then((counties) => {
-      setData(counties);
-    });
+
   }, []);
 
   return (
-    <>
-      <ComposableMap projection="geoAlbersUsa">
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const cur = data.find((s) => s.id === geo.id);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={colorScale(cur ? cur.unemployment_rate : "#EEE")}
-                />
-              );
-            })
-          }
-        </Geographies>
-      </ComposableMap>
-    </>
+    <ComposableMap
+      projection="geoAzimuthalEqualArea"
+      projectionConfig={{
+        rotate: [58, 20, 0],
+        scale: 400
+      }}
+    >
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => {
+            const state = brasil.objects.estados.geometries.find(state => geo.id === state.id)
+            return (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill={state.region === region ? '#C5E2B7' : '#E0E5DF'}
+                stroke="#fff"
+              />
+            )
+          })
+        }
+      </Geographies>
+      {markers.map(({ name, coordinates, markerOffset }) => (
+        <Marker key={name} coordinates={coordinates}>
+          <g
+            fill="none"
+            stroke="#FF5533"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            transform="translate(-12, -24)"
+          >
+            <circle cx="12" cy="10" r="3" />
+            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+          </g>
+          <text
+            textAnchor="middle"
+            y={markerOffset}
+            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+          >
+            {name}
+          </text>
+        </Marker>
+      ))}
+    </ComposableMap>
   );
 };
 
